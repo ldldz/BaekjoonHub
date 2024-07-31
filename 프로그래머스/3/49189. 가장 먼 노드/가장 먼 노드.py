4957d8b8641eeedd7653a0e6a0ca91bf2690ad30
Geon <qkrqudrjs4350@gmail.com>
2024-07-31 13:52:40 +0900

    [level 3] Title: 가장 먼 노드, Time: 51.85 ms, Memory: 20.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,24 @@
+def solution(n, edge):
+    ans = 0
+    graph = [[] for i in range(n+1)]
+    queue = [1]
+    visited = [False] * (n + 1)
+    visited[1] = True
+    
+    for [v1, v2] in edge:
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+    
+    while queue:
+        next_queue = []
+        
+        for v in queue:
+            for i in graph[v]:
+                if not visited[i]:
+                    visited[i] = True
+                    next_queue.append(i)
+                    
+        if len(next_queue): ans = len(next_queue)
+        queue = next_queue
+        
+    return ans
