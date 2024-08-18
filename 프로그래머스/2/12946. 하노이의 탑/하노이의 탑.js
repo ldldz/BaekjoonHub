d1fb10bf37e8d7d8f1626f2bb2aae265dc80d747
Geon <qkrqudrjs4350@gmail.com>
2024-08-18 11:07:17 +0900

    [level 2] Title: 하노이의 탑, Time: 15.93 ms, Memory: 49.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,9 +1,5 @@
-function f(n, a, b) {
-    if (n === 1) return [[a, b]];
-    const [m] = [1, 2, 3].filter(v => v !== a && v !== b);
-    return [...f(n - 1, a, m), [a, b], ...f(n - 1, m, b)]
-}
+const f = (n, a, b, c) => n === 1 ? [[a, c]] : [...f(n - 1, a, c, b), [a, c], ...f(n - 1, b, a, c)];
 
 function solution(n) {
-    return f(n, 1, 3);
+    return f(n, 1, 2, 3);
 }
