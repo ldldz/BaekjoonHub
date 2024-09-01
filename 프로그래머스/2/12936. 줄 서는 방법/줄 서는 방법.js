6592a121a3c9b8b7ca2232324be4ef98a1420a54
Geon <qkrqudrjs4350@gmail.com>
2024-09-02 07:44:12 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.27 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -0,0 +1,22 @@
+function facto(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result
+}
+
+function solution(n, k) {
+    const ans = [];
+    let arr = Array.from({ length: n }, (_, i) => i + 1);
+    
+    for (let i = n - 1; i > 0; i--) {
+        const f = facto(i);
+        const j = Math.ceil(k / f) || arr.length;
+        ans.push(arr[j - 1]);
+        k %= f;
+        arr = arr.filter((v) => v !== arr[j - 1]);
+    }
+    
+    return [...ans, arr[0]];
+}
